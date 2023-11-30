bbec810835616375483a1792c8e0dfd5e3f818a9
github_kks0509 <ks509koo@gmail.com>
2023-12-01 04:03:12 +0900

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,158 @@
+'use client';
+
+//메인 홈 화면
+
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import Link from 'next/link';
+
+export default function Home() {
+
+  const [products] = useState([]);
+  const router = useRouter();
+
+  console.log(products);
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <section className="">
+        <div className="grid max-w-screen-xl px-4 py-8 mx-suto  lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+          <div className="mr-auto place-self-center lg:col-span-7">
+            <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl">
+              Best Shopping Website
+            </h1>
+            <p className="max-w-2xl mb-6 font-bold text-gray-500 lg:mb-8 md:text-lg lg:text-xl">
+              Z.P.G is a humble group of enthusiasts hoping to assist the
+              journey of seizing your passion with fashion.
+            </p>
+
+            <button
+              type="button"
+              onClick={() => router.push('/product/products')}
+              className="mt-30 inline-block bg-gray-700 px-5 py-3 text-xs font-medium uppercase tracking-wide text-white"
+            >
+              go shopping
+            </button>
+          </div>
+          <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
+            <img src="1.jpg" className="h-1500 w-1500 mx-4 my-4" />
+          </div>
+        </div>
+        <div className="max-w-screen-xl px-4 py-8 mx-auto sm:py-12 sm:px-6 lg:px-8">
+          <div className="grid grid-cols-1 gap-4 lg:grid-cols-3 lg:items-stretch">
+            <div className="grid p-6 bg-gray-100 rounded place-content-center sm:p-8">
+              <div className="max-w-md mx-auto text-center lg:text-left">
+                <div>
+                  <h2 className="text-xl font-bold text-gray-900 sm:text-3xl">
+                  Do you have any questions?
+                  </h2>
+                </div>
+                <button
+                  onClick={() => router.push('/product/chat')}
+                  className="mt-1.5 inline-block bg-gray-700 px-5 py-3 text-xs font-medium uppercase tracking-wide text-white"
+                >
+                  go chat!
+                </button>
+              </div>
+            </div>
+            <div className="lg:col-span-2 lg:py-8">
+              <ul className="grid grid-cols-2 gap-4">
+                {products && products.length
+                  ? products
+                      .filter((item) => item.onSale === 'yes')
+                      .splice(0, 2)
+                      .map((productItem) => (
+                        <li
+                          onClick={() =>
+                            router.push(`/product/${productItem._id}`)
+                          }
+                          className="cursor-pointer"
+                          key={productItem._id}
+                        >
+                          <div>
+                            <img
+                              src={productItem.imageUrl}
+                              alt="Sale Product Item"
+                              className="object-cover w-full rounded aspect-square"
+                            />
+                          </div>
+                          <div className="mt-3">
+                            <h3 className="font-medium text-gray-900">
+                              {productItem.name}
+                            </h3>
+                            <p className="mt-1 text-sm text-gray-800">
+                              ${productItem.price}{' '}
+                              <span className="text-red-700">{`(-${productItem.priceDrop}%) Off`}</span>
+                            </p>
+                          </div>
+                        </li>
+                      ))
+                  : null}
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="max-w-screen-xl px-4 py-8 mx-auto sm:px-6 sm:py-12 lg:px-8">
+          <div className="text-center">
+            <h2 className="text-xl font-bold text-gray-950 sm:text-3xl">
+              CATEGORY
+            </h2>
+          </div>
+          <ul className="grid grid-cols-1 gap-4 mt-8 lg:grid-cols-3">
+            <li>
+              <div className="relative block group">
+                <img
+                  src="2.jpg"
+                  className="object-cover w-full aspect-square"
+                />
+                <div className="absolute inset-0 flex flex-col items-start justify-end p-6">
+                  <h3 className="text-xl font-medium text-white">Products</h3>
+                  <button
+                    onClick={() => router.push('/product/products')}
+                    className="mt-1.5 inline-block bg-gray-700 px-5 py-3 text-xs font-medium uppercase tracking-wide text-white"
+                  >
+                    Shop Now
+                  </button>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className="relative block group">
+                <img
+                  src="woman.jpg"
+                  className="object-cover w-full aspect-square"
+                />
+                <div className="absolute inset-0 flex flex-col items-start justify-end p-6">
+                  <h3 className="text-xl font-medium text-white">WOMAN</h3>
+                  <button
+                    onClick={() => router.push('/product/woman')}
+                    className="mt-1.5 inline-block bg-gray-700 px-5 py-3 text-xs font-medium uppercase tracking-wide text-white"
+                  >
+                    Shop Now
+                  </button>
+                </div>
+              </div>
+            </li>
+            <li className="lg:col-span-2 lg:col-start-2 lg:row-span-2 lg:row-start-1">
+              <div className="relative block group">
+                <img
+                  src="man.jpg"
+                  className="object-cover w-full aspect-square"
+                />
+                <div className="absolute inset-0 flex flex-col items-start justify-end p-6">
+                  <h3 className="text-xl font-medium text-white">MAN</h3>
+                  <button
+                    onClick={() => router.push('/product/man')}
+                    className="mt-1.5 inline-block bg-gray-700 px-5 py-3 text-xs font-medium uppercase tracking-wide text-white"
+                  >
+                    Shop Now
+                  </button>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </section>
+    </main>
+  );
+}
---
+++ src/app/product/woman/page.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Woman() {
+  return (
+    <div>1. 웹프 4번 PPT 참고해서 제작<br></br>사진은 public에 Woman_1.jpg 이런 식으로 순서대로 넣으면 됩니다.</div>
+  )
+}
