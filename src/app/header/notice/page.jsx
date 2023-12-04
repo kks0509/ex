f89df9cee07327193fac9802cd5d952d3083564a
github_kks0509 <ks509koo@gmail.com>
2023-12-04 17:19:08 +0900

    commit

--- src/app/header/notice/page.jsx
+++ src/app/header/notice/page.jsx
@@ -1,21 +1,47 @@
-import ProductItem from '@/components/ProductItem'
-import { data } from '@/utils/data'
-export default function Home() {
-  const { notice } = data
+//className="transform hover:scale-125 transition duration-2000 ease-out"
+//위 코드는 마우스를 올렸을 때 대상 이미지가 움직이도록 하는 설정
 
+import React from 'react';
+
+export default function Notice() {
   return (
-    <div>
-      <div className="max-w-9xl mx-16 h-90 bg-white dark:bg-gray-800 text-left md:text-left">
-        <h1 className="text-9xl md:text-16xl font-bold py-20">
-          Notce
-        </h1>
+      <div className="max-w-6xl mx-auto">
+        <div className="grid md:grid-cols-3 gap-20 lg:-mt-8 pb-40">
+          <br></br>
+          <a
+            href="/app/header/notice/content/page1"
+            className="w-full block col-span-3 shadow-2xl"
+          >
+            <div className="relative overflow-hidden">
+              <img
+                src="/event.jpg"
+                alt="event1"
+                className="transform hover:scale-125 transition duration-2000 ease-out"
+              />
+              <h1 className="absolute top-10 left-10 text-gray-50 font-bold text-xl bg-red-500 rounded-md px-2">
+                Event 1
+              </h1>
+            </div>
+          </a>
+          <a
+            href="/app/header/notice/content/page2"
+            className="w-full block col-span-3  sm:col-span-2 shadow-2xl"
+          >
+            <div className="relative overflow-hidden">
+              <img
+                src="/event2.png"
+                alt="event2"
+                className="transform hover:scale-125 transition duration-2000 ease-out"
+              />
+              <h1 className="absolute top-10 left-10 text-gray-50 font-bold text-xl bg-red-500 rounded-md px-2">
+                event2
+              </h1>
+
+            </div>
+          </a>
+          
+        </div>
       </div>
-      <div className="grid grid-cols-1 gap-10 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-6
-      "> 
-        {notice.map((product) => (
-          <ProductItem key={product.id} product={product} />
-        ))}
-      </div>
-    </div>
-  )
+
+  );
 }
